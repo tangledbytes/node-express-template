8680ec67b4652ade158867aea14a6e568dd151fa
Utkarsh Srivastava <srivastavautkarsh8097@gmail.com>
2020-10-18 21:32:26 +0530

    Moved linting to jest

--- jest.config.js
+++ jest.config.js
@@ -1,10 +1,10 @@
-const { pathsToModuleNameMapper } = require('ts-jest/utils');
-const { compilerOptions } = require('./tsconfig');
-
 module.exports = {
-  preset: 'ts-jest',
-  testEnvironment: 'node',
-  moduleNameMapper: pathsToModuleNameMapper(compilerOptions.paths, {
-    prefix: '<rootDir>/',
-  }),
+  collectCoverageFrom: ['**/src/**/*.ts'],
+  coverageThreshold: {
+    // Change the threshold as the coverage grows
+    global: { statements: 30, branches: 30, functions: 30, lines: 30 },
+    // Add per glob coverage threshold here
+  },
+  projects: ['./jest.app.js', './jest.lint.js'],
+  watchPlugins: ['jest-watch-select-projects'],
 };
