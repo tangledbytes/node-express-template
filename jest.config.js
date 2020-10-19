28683fd115ff2210038bd29c89cfdef3ef5adf51
Utkarsh Srivastava <srivastavautkarsh8097@gmail.com>
2020-10-19 16:11:02 +0530

    Updated threshold

--- jest.config.js
+++ jest.config.js
@@ -2,7 +2,7 @@ module.exports = {
   collectCoverageFrom: ['**/src/**/*.ts'],
   coverageThreshold: {
     // Change the threshold as the coverage grows
-    global: { statements: 10, branches: 2, functions: 10, lines: 10 },
+    global: { statements: 30, branches: 20, functions: 30, lines: 30 },
     // Add per glob coverage threshold here
   },
   projects: ['./test/jest.app.js', './test/jest.lint.js'],
